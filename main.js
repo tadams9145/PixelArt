77856665f6123dc867fa22a7aeece102e420a11f
tadams9145 <tadams9145@gmail.com>
2018-10-15 11:13:25 -0600

    Updated

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
-let chosenColor = "";
-
 //Handle Pixel Click
 
 let container = document.querySelector(".container")
-
-
 function handleClick(event){
-   console.log(event);
-   event.target.style.backgroundColor = chosenColor;
+   event.target.style.backgroundColor = chosenColor
 }
-
-container.addEventListener("click", handleClick);
-
+container.addEventListener("click", handleClick)
 //Handle Color Selection
-
 let colors = document.querySelector(".colors")
-
 function handleColorSelection(event){
-   chosenColor = event.target.classList[1];
-   console.log("chosenColor", chosenColor);
+   chosenColor = event.target.classList[1]
+
 }
+colors.addEventListener("click", handleColorSelection)
+for(let i = 0;i < 20;i++){
+   let block = document.createElement("div")
+   let tr = document.createElement("tr")
+   block.className = "block"
+   container.appendChild(block)
+   block.appendChild(tr)
 
-colors.addEventListener("click", handleColorSelection);
+   for(let j = 0; j < 50; j++){
+     let td = document.createElement("td")
+     tr.appendChild(td)
+     td.className = "pixel"
 
-for(let i = 0;i < 584;i++){
-   let pixel = document.createElement("div")
-   pixel.className = "pixel"
-   container.appendChild(pixel)
+   }
 
 }
